7488c771f323af91660fd9d79e161afccc78625d
Benjamin Kearney <benjaminjameskearney@gmail.com>
2020-08-16 16:24:54 -0400

    move requests to seperate module

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from chalice import Chalice, Rate
-import requests
+from chalicelib import req
 import boto3
 import time
 import json
@@ -12,26 +12,27 @@ Return Weather Data for Montreal
 '''
 @app.route('/')
 def index():
-    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
-    data = response.json()
-    return data
+    #response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
+    #data = response.json()
+    return req.request()
 '''
 Return Weather Data for the City defined in the Route
 '''
 @app.route('/{city}')
 def weatherByCityName(city):
-    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=9ee255b8e671643ab089e28c1ef7a303"
-    url = url.format(city)
-    response = requests.get(url)
-    data = response.json()
-    return data
+    #url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=9ee255b8e671643ab089e28c1ef7a303"
+    #url = url.format(city)
+    #response = requests.get(url)
+    #data = response.json()
+    return req.requestCity(city)
 '''
 Uploads curent weather data to S3 every minute
 '''
 @app.schedule(Rate(5, unit=Rate.MINUTES))
 def periodic_task(event):
-    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
-    data = response.json()
+    #response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
+    #data = response.json()
+    data = req.request()
     name = str(round(time.time()))
 
     f = open("/tmp/weatherdata", "w")
@@ -44,18 +45,4 @@ def periodic_task(event):
     except:
         return {"Success": "False"}
 
-@app.route('/testS3')
-def test_S3():
-    response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
-    data = response.json()
-    name = str(round(time.time()))
 
-    f = open("/tmp/weatherdata", "w")
-    f.write(json.dumps(data))
-    f.close()
-
-    try:
-        s3.upload_file("/tmp/weatherdata", "weather-app-data", name)
-        return {"Success": "True"}
-    except:
-        return {"Success": "False"}
---
+++ chalicelib/req.py
@@ -0,0 +1,10 @@
+import requests
+
+def request():
+    return requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
+
+
+def requestCity(name):
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=9ee255b8e671643ab089e28c1ef7a303"
+    url = url.format(name)
+    return requests.get(url)
