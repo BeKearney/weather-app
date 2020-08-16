04b4d5ebc961716dee185073bcc311683fe3e587
Benjamin Kearney <benjaminjameskearney@gmail.com>
2020-08-16 16:30:20 -0400

    fix json bug

--- chalicelib/req.py
+++ chalicelib/req.py
@@ -1,10 +1,10 @@
 import requests
 
 def request():
-    return requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
+    return requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303").json()
 
 
 def requestCity(name):
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=9ee255b8e671643ab089e28c1ef7a303"
     url = url.format(name)
-    return requests.get(url)
+    return requests.get(url).json()
