f5b8e2a843edf391d80c9345c1e7d9a9844b77c0
Benjamin Kearney <benjaminjameskearney@gmail.com>
2020-08-16 16:32:31 -0400

    remove commented out code

--- app.py
+++ app.py
@@ -12,26 +12,18 @@ Return Weather Data for Montreal
 '''
 @app.route('/')
 def index():
-    #response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
-    #data = response.json()
     return req.request()
 '''
 Return Weather Data for the City defined in the Route
 '''
 @app.route('/{city}')
 def weatherByCityName(city):
-    #url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=9ee255b8e671643ab089e28c1ef7a303"
-    #url = url.format(city)
-    #response = requests.get(url)
-    #data = response.json()
     return req.requestCity(city)
 '''
 Uploads curent weather data to S3 every minute
 '''
 @app.schedule(Rate(5, unit=Rate.MINUTES))
 def periodic_task(event):
-    #response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=9ee255b8e671643ab089e28c1ef7a303")
-    #data = response.json()
     data = req.request()
     name = str(round(time.time()))
 
